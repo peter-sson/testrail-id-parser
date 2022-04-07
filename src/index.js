1a989acf0b9f83d3b5cfa570abcb709b9f22aef4
Peter Son <pson@skillz.com>
2022-04-07 16:39:42 -0700

    parse int for plan id

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import core from "@actions/core"
+import core from "@actions/core";
 import recursiveReadDir from 'recursive-readdir';
 import { readFile as _readFile } from 'fs';
 import Testrail from 'testrail-api';
 
 const testrail_url = core.getInput('testrail-url');
-const testrailPlanId = core.getInput('testrail-plan-id')
+const testrailPlanId = parseInt(core.getInput('testrail-plan-id'));
 const TESTRAIL_API_KEY = core.getInput('TESTRAIL_API_KEY');
-const TESTRAIL_USER = core.getInput('TESTRAIL_USER')
+const TESTRAIL_USER = core.getInput('TESTRAIL_USER');
 const test_directory = core.getInput('test-directory');
 
 
@@ -23,37 +23,37 @@ async function run() {
 
     // Get test `@CXXXXX` test ids 
     const regex = /[\@C][0-9]+/g
-    const featureFiles = await recursiveReadDir(test_directory, ['!*.feature'])
+    const featureFiles = await recursiveReadDir(test_directory, ['!*.feature']);
     
     for (const file of featureFiles) {
         const constents = await readFile(file);
         const newData = constents.replace(/\n/g, " ");
-        repoTestIds.push(...newData.match(regex))
+        repoTestIds.push(...newData.match(regex));
     }
 
     // Get testrail template test ids
-    console.log(`Getting testrail test ids from plan id: ${testrailPlanId}...`)
+    console.log(`Getting testrail test ids from plan id: ${testrailPlanId}...`);
     const testrailPlan = await testrail.getPlan(/*RUN_ID=*/testrailPlanId);
 
     const testrailRunIds = []
     for (const entry of testrailPlan.body.entries) {
-        testrailRunIds.push(...entry.runs.map(x => x.id))
+        testrailRunIds.push(...entry.runs.map(x => x.id));
     }
 
     for (const run of testrailRunIds) {
-        const test = await testrail.getTests(run)
-        testrailIds.push(...test.body.tests.map(x => x.case_id))
+        const test = await testrail.getTests(run);
+        testrailIds.push(...test.body.tests.map(x => x.case_id));
     }
 
     // Find test ids not included in testrail template
     for (const id of repoTestIds) {
-        const idInt = parseInt(id.substring(1))
+        const idInt = parseInt(id.substring(1));
         if (!testrailIds.includes(idInt)) {
             missingIds.push(id);
         }
     }
     
-    core.setOutput('missing-ids', missingIds)
+    core.setOutput('missing-ids', missingIds);
 }
 
 run();
