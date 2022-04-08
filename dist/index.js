3f8c9076634955aa0cdd56a46e0bbf7b012432c5
Peter Son <pson@skillz.com>
2022-04-08 10:10:29 -0700

    remove test code

--- dist/index.js
+++ dist/index.js
@@ -42522,8 +42522,6 @@ const testrail = new Testrail({
 });
 
 async function run() {
-    const test = ["A", "B"]
-    console.log(test)
     const repoTestIds = [] // Collection of target repo's test ids from cucumber tag.
     let testrailIds = [] // Collection of targe testrail plan's test ids.
     const missingIds = [] // Collection of repo's test ids not included in testrail plan.
